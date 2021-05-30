4cba6ef3b30be332200081048998d43ca5693386
Mhd. Dwi Al Fiqri <screamingwazozky@gmail.com>
2021-05-30 16:00:25 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,96 @@
+const express = require('express')
+
+const mysql = require('mysql')
+
+
+
+const app = express()
+
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.json())
+
+
+
+const connection = mysql.createConnection({
+
+    host            : 'localhost',
+
+    user            : 'root',
+
+    password        : '',
+
+    database        : 'tugas1'
+
+})
+
+
+
+connection.connect();
+
+
+
+app.get('/', (req,res) => {
+
+    res.send(`<html>
+
+        <body>
+
+            <form action="/todo" method="post">
+
+                <input name="deskripsi" />
+
+                <button>Add</button>
+
+            </form>
+
+        </body>
+
+    </html>`)
+
+})
+
+
+
+app.post('/todo', (req,res) => {
+
+    var params = req.body.deskripsi
+
+        connection.query("insert into table1 values(null, ?)", params, (err, rows, fields) =>{
+
+            if(err) throw err
+
+                res.end()
+
+        })
+
+})
+
+
+
+app.get('/todo', (req, res, fields) => {
+
+        connection.query('SELECT Deskripsi from table1', (err, rows, fields) =>{
+
+            if(err) throw err
+
+            rows = JSON.stringify(rows);
+
+            res.send(rows)
+
+        })
+
+})
+
+app.delete('/todo/:id', (req, res) => {
+    var sql = `DELETE FROM todo WHERE id = ${req.params.id}`;
+	con.query(sql)
+	res.end()
+})
+
+app.listen(3000, function(err){
+
+    console.log("Server started");
+
+})
+
